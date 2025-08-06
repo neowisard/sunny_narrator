df7ddda625851a6ddfb6c8a04d8360d364a27f06
Neow15ard <n@uwns.org>
2025-08-06 15:41:19 +0300

    few books batch , if .dic ready

--- batched.sh
+++ batched.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #plan llm pairs:  qwen3-32_ mistral ,
-# Определение списков значений
+
 shorts=("Empire" "Exodus" "First")
 #"mi24_q3-32" "q3-32_g3_27b" "g3_27b_m24" "m24_g3_27b" "m24_s3_14b" "saiyax8b_saig3_14b")
 file=("books/Empire.fb2" "books/Exodus.fb2" "books/First.fb2")
